09f1fcb5ecbb6e1593ae387c45c51033565a3b93
Giacomo Lentini <g.lentini2@studenti.unisa.it>
2022-03-10 18:05:18 +0100

    -DESIGN/SCHELETRI COMPLETI -ICONE BASIC IN BASSO AGGIUSTATE -IMMAGINI DELLE SPECIE E CLASSE ANIMALE -AGGIUNTA FOTO IN DRAWABLE -AGGIUNTA CLASSI+XML: *descrizione animale *lista progetti *descrizione progetto (Tutte da riguardare per bene)

---
+++ app/src/main/java/com/example/myswanis/DescrAnimal.java
@@ -0,0 +1,15 @@
+package com.example.myswanis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class DescrAnimal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.descrizione_animal);
+    }
+}
