b0dbf06c7cc9b39b44ceb883f20e2fc7d84d458c
Giacomo Lentini <g.lentini2@studenti.unisa.it>
2022-03-22 22:46:22 +0100

    -DESIGN/SCHELETRI COMPLETI -ICONE BASIC IN BASSO AGGIUSTATE -IMMAGINI DELLE SPECIE E CLASSE ANIMALE -AGGIUNTA FOTO IN DRAWABLE -AGGIUNTA CLASSI+XML: *descrizione animale *lista progetti *descrizione progetto (Tutte da riguardare per bene) -Funzioni degli imagebutton iniziali per cliccare una specie

--- app/src/main/java/com/example/myswanis/MainActivity.java
+++ app/src/main/java/com/example/myswanis/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.myswanis;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageButton button1 = (ImageButton)findViewById(R.id.imageView5);
+        ImageButton button2 = (ImageButton)findViewById(R.id.imageView6);
+
+        button1.setOnClickListener(
+                new ImageButton.OnClickListener() {
+                    public void onClick(View v) {
+                        Intent openPage1 = new Intent(MainActivity.this,SpecificyClass2.class);
+                        startActivity(openPage1);
+                    };
+            });
+        button2.setOnClickListener(
+                new ImageButton.OnClickListener() {
+                    public void onClick(View v) {
+                        Intent openPage2 = new Intent(MainActivity.this,SpecificyClass.class);
+                        startActivity(openPage2);
+                    };
+                });
     }
 }
+
