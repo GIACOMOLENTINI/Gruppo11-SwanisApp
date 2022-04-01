7156054ebc62c9f6d791fd17dc8dffccc80b1422
Giacomo Lentini <g.lentini2@studenti.unisa.it>
2022-04-01 12:42:43 +0200

    -DESIGN/SCHELETRI COMPLETI -ICONE BASIC IN BASSO AGGIUSTATE -IMMAGINI DELLE SPECIE E CLASSE ANIMALE -AGGIUNTA FOTO IN DRAWABLE -AGGIUNTA CLASSI+XML: *descrizione animale *lista progetti *descrizione progetto (Tutte da riguardare per bene) -Funzioni degli imagebutton iniziali per cliccare una specie
    ---PRIME 5/6 CLASSI INSERITE NEL MANIFEST
    ---MIGLIORAMENTO CLASSILAYOUT.XML
    ---PRIMO POPUP PROVATO
    ---AGGIUNTO FILE STYLES PER DARE A POP UP APPARENZA POCHI SECONDI

--- app/src/main/java/com/example/myswanis/CompilaClass.java
+++ app/src/main/java/com/example/myswanis/CompilaClass.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class CompilaClass extends AppCompatActivity {
 
@@ -14,6 +15,9 @@ public class CompilaClass extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.compila_layout);
         Button button3 = (Button)findViewById(R.id.button);
+        ImageButton button4 = (ImageButton)findViewById(R.id.imageButton21);
+        ImageButton button5 = (ImageButton)findViewById(R.id.imageButton21);
+        ImageButton button6 = (ImageButton)findViewById(R.id.imageButton22);
 
 
         button3.setOnClickListener(
@@ -21,6 +25,27 @@ public class CompilaClass extends AppCompatActivity {
                     public void onClick(View v) {
                         Intent openPage3 = new Intent(CompilaClass.this,Pop1.class);
                         startActivity(openPage3);
+                    };
+                });
+        button4.setOnClickListener(
+                new ImageButton.OnClickListener() {
+                    public void onClick(View v) {
+                        Intent openPage2 = new Intent(CompilaClass.this,ProjectActivity.class);
+                        startActivity(openPage2);
+                    };
+                });
+        button5.setOnClickListener(
+                new ImageButton.OnClickListener() {
+                    public void onClick(View v) {
+                        Intent openPage2 = new Intent(CompilaClass.this,MainActivity.class);
+                        startActivity(openPage2);
+                    };
+                });
+        button6.setOnClickListener(
+                new ImageButton.OnClickListener() {
+                    public void onClick(View v) {
+                        Intent openPage2 = new Intent(CompilaClass.this,MainActivity2.class);
+                        startActivity(openPage2);
                     };
                 });
     }
