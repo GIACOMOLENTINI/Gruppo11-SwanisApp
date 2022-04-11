34ea53d2d8a169f38c298cda31857be788a88634
Giacomo Lentini <g.lentini2@studenti.unisa.it>
2022-04-11 18:29:47 +0200

    Punto 5 fatto, modificato scrollview con layout Punto 6 fatto, primo toast completato, successivi, copia incolla Inserito pagina di LOGIN

--- app/src/main/java/com/example/myswanis/ProjectClass.java
+++ app/src/main/java/com/example/myswanis/ProjectClass.java
@@ -2,8 +2,12 @@ package com.example.myswanis;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class ProjectClass extends AppCompatActivity {
 
@@ -11,5 +15,42 @@ public class ProjectClass extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.descrizione_progetto);
+
+        Button button3 = (Button)findViewById(R.id.button);
+        ImageButton button4 = (ImageButton)findViewById(R.id.imageButton21);
+        ImageButton button5 = (ImageButton)findViewById(R.id.imageButton21);
+        ImageButton button6 = (ImageButton)findViewById(R.id.imageButton22);
+
+
+        button3.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+                        Toast.makeText(getApplicationContext(), "PARTECIPAZIONE EFFETTUATA!!!" , Toast.LENGTH_LONG).show();
+                        Intent openPage1 = new Intent(ProjectClass.this,MainActivity.class);
+                        startActivity(openPage1);
+                    };
+                });
+        button4.setOnClickListener(
+                new ImageButton.OnClickListener() {
+                    public void onClick(View v) {
+                        Intent openPage2 = new Intent(ProjectClass.this,ProjectActivity.class);
+                        startActivity(openPage2);
+                    };
+                });
+        button5.setOnClickListener(
+                new ImageButton.OnClickListener() {
+                    public void onClick(View v) {
+                        Intent openPage2 = new Intent(ProjectClass.this,MainActivity.class);
+                        startActivity(openPage2);
+                    };
+                });
+        button6.setOnClickListener(
+                new ImageButton.OnClickListener() {
+                    public void onClick(View v) {
+                        Intent openPage2 = new Intent(ProjectClass.this,MainActivity2.class);
+                        startActivity(openPage2);
+                    };
+                });
+
     }
 }
