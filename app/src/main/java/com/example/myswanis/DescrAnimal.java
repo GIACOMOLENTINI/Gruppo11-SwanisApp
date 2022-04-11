77d5e1543b96250c4606e487f6c35ae1d8fc4d63
Giacomo Lentini <g.lentini2@studenti.unisa.it>
2022-04-11 18:18:06 +0200

    Punto 5 fatto, modificato scrollview con layout Punto 6 fatto, primo toast completato, successivi, copia incolla Inserito pagina di LOGIN

--- app/src/main/java/com/example/myswanis/DescrAnimal.java
+++ app/src/main/java/com/example/myswanis/DescrAnimal.java
@@ -2,8 +2,10 @@ package com.example.myswanis;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 
 public class DescrAnimal extends AppCompatActivity {
 
@@ -11,5 +13,32 @@ public class DescrAnimal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.descrizione_animal);
+
+        ImageButton button4 = (ImageButton)findViewById(R.id.imageButton20);
+        ImageButton button5 = (ImageButton)findViewById(R.id.imageButton21);
+        ImageButton button6 = (ImageButton)findViewById(R.id.imageButton22);
+
+        button4.setOnClickListener(
+                new ImageButton.OnClickListener() {
+                    public void onClick(View v) {
+                        Intent openPage2 = new Intent(DescrAnimal.this,ProjectActivity.class);
+                        startActivity(openPage2);
+                    };
+                });
+        button5.setOnClickListener(
+                new ImageButton.OnClickListener() {
+                    public void onClick(View v) {
+                        Intent openPage2 = new Intent(DescrAnimal.this,MainActivity.class);
+                        startActivity(openPage2);
+                    };
+                });
+        button6.setOnClickListener(
+                new ImageButton.OnClickListener() {
+                    public void onClick(View v) {
+                        Intent openPage2 = new Intent(DescrAnimal.this,MainActivity2.class);
+                        startActivity(openPage2);
+                    };
+                });
+
     }
 }
