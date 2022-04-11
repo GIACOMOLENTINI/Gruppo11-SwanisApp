16b8b8925399d9e5572be589459bbcba0b3f88d7
Giacomo Lentini <g.lentini2@studenti.unisa.it>
2022-04-11 17:13:29 +0200

    Punto 5 fatto, modificato scrollview con layout Punto 6 fatto, primo toast completato, successivi, copia incolla Inserito pagina di LOGIN

---
+++ app/src/main/java/com/example/myswanis/Login.java
@@ -0,0 +1,50 @@
+package com.example.myswanis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+        TextView username =(TextView) findViewById(R.id.username);
+        TextView password =(TextView) findViewById(R.id.password);
+
+        MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
+
+        //admin and admin
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(username.getText().toString().equals("Flavia") && password.getText().toString().equals("flavia")){
+                    //correct
+                    Toast.makeText(Login.this,"LOGIN SUCCESSFUL",Toast.LENGTH_SHORT).show();
+                    Intent openPage1 = new Intent(Login.this,MainActivity2.class);
+                    startActivity(openPage1);
+                    if(username.getText().toString().equals("Francesco") && password.getText().toString().equals("francesco")){
+                        //correct
+                        Toast.makeText(Login.this,"LOGIN SUCCESSFUL",Toast.LENGTH_SHORT).show();
+                        Intent openPage2 = new Intent(Login.this,MainActivity.class);
+                        startActivity(openPage2);
+                }else
+                    //incorrect
+                    Toast.makeText(Login.this,"LOGIN FAILED !!!",Toast.LENGTH_SHORT).show();
+            }
+        }});
+
+
+    }
+}
