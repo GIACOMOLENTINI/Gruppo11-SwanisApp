7f143a0e72131726bcf2c8c3c65c70d45d69af89
Giacomo Lentini <g.lentini2@studenti.unisa.it>
2022-05-31 21:49:40 +0200

    -Pagina Login migliorata con sign in e forgot password -Aggiunta Logo con pagina iniziale di apertura app -Aggiunta bottone Logout

--- app/src/main/java/com/example/myswanis/DescrProfileFL.java
+++ app/src/main/java/com/example/myswanis/DescrProfileFL.java
@@ -3,6 +3,7 @@ package com.example.myswanis;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
@@ -18,6 +19,7 @@ public class DescrProfileFL extends AppCompatActivity {
         ImageButton button4 = (ImageButton)findViewById(R.id.imageButton20);
         ImageButton button5 = (ImageButton)findViewById(R.id.imageButton21);
         ImageButton button6 = (ImageButton)findViewById(R.id.imageButton22);
+        Button button7 = (Button)findViewById(R.id.button);
 
        button4.setOnClickListener(
                 new ImageButton.OnClickListener() {
@@ -37,6 +39,13 @@ public class DescrProfileFL extends AppCompatActivity {
                 new ImageButton.OnClickListener() {
                     public void onClick(View v) {
                         Toast.makeText(DescrProfileFL.this,"ALREADY ON PROFILE PAGE",Toast.LENGTH_LONG).show();
+                    };
+                });
+        button7.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+                        Intent openPage2 = new Intent(DescrProfileFL.this,Login.class);
+                        startActivity(openPage2);
                     };
                 });
 
