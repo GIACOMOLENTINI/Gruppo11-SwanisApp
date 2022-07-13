507c24845cfd2c003a5b2268959f45bb56bc87d2
Giacomo Lentini <g.lentini2@studenti.unisa.it>
2022-07-13 23:08:20 +0200

    - Aggiunta delle classi DescrAnimal2FL, DescrAnimalFL e MapAnimalFL - Modifica di tutti i collegamenti ( da riguardare soltanto se saltati qualcuno) - Modifica descrizione Lepre con aggiunta foto - modifica nome immagini nuove

---
+++ app/src/main/java/com/example/myswanis/GestProgetti.java
@@ -0,0 +1,53 @@
+package com.example.myswanis;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GestProgetti extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gest_progetti);
+
+        ImageButton button4 = (ImageButton)findViewById(R.id.imageButton20);
+        ImageButton button5 = (ImageButton)findViewById(R.id.imageButton21);
+        ImageButton button6 = (ImageButton)findViewById(R.id.imageButton22);
+        ImageButton button9 = (ImageButton)findViewById(R.id.mod_button);
+
+        button4.setOnClickListener(
+                new ImageButton.OnClickListener() {
+                    public void onClick(View v) {
+                        Intent openPage1 = new Intent(GestProgetti.this,ProjectActivityManage.class);
+                        startActivity(openPage1);
+                    };
+                });
+        button5.setOnClickListener(
+                new ImageButton.OnClickListener() {
+                    public void onClick(View v) {
+                        Intent openPage2 = new Intent(GestProgetti.this,MainActivity2.class);
+                        startActivity(openPage2);
+                    };
+                });
+        button6.setOnClickListener(
+                new ImageButton.OnClickListener() {
+                    public void onClick(View v) {
+                        Intent openPage2 = new Intent(GestProgetti.this,DescrProfileFL.class);
+                        startActivity(openPage2);
+                    };
+                });
+        button9.setOnClickListener(
+                new ImageButton.OnClickListener() {
+                    public void onClick(View v) {
+                        Intent openPage2 = new Intent(GestProgetti.this, GestProgetti2.class);
+                        startActivity(openPage2);
+                    };
+                });
+    }
+}
---
+++ app/src/main/java/com/example/myswanis/GestProgetti2.java
@@ -0,0 +1,55 @@
+package com.example.myswanis;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GestProgetti2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gest_progetti);
+
+        ImageButton button4 = (ImageButton)findViewById(R.id.imageButton20);
+        ImageButton button5 = (ImageButton)findViewById(R.id.imageButton21);
+        ImageButton button6 = (ImageButton)findViewById(R.id.imageButton22);
+        Button button9 = (Button)findViewById(R.id.button);
+
+        button4.setOnClickListener(
+                new ImageButton.OnClickListener() {
+                    public void onClick(View v) {
+                        Intent openPage1 = new Intent(GestProgetti2.this,ProjectActivityManage.class);
+                        startActivity(openPage1);
+                    };
+                });
+        button5.setOnClickListener(
+                new ImageButton.OnClickListener() {
+                    public void onClick(View v) {
+                        Intent openPage2 = new Intent(GestProgetti2.this,MainActivity2.class);
+                        startActivity(openPage2);
+                    };
+                });
+        button6.setOnClickListener(
+                new ImageButton.OnClickListener() {
+                    public void onClick(View v) {
+                        Intent openPage2 = new Intent(GestProgetti2.this,DescrProfileFL.class);
+                        startActivity(openPage2);
+                    };
+                });
+        button9.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+                        Toast.makeText(getApplicationContext(), "PROGETTO MODIFICATO CORRETTAMENTE!!!" , Toast.LENGTH_LONG).show();
+                        Intent openPage2 = new Intent(GestProgetti2.this, GestProgetti.class);
+                        startActivity(openPage2);
+                    };
+                });
+    }
+}
